f1b9efbc935a6f362abbf64a7ec3423aa6112de6
Eric Kennedy <ekeric13@gmail.com>
2016-07-18 09:09:51 -0700

    Serve up static file

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import Koa from 'koa';
+import serve from 'koa-static';
 
 const server = new Koa();
 const port = process.env.PORT || 3000;
 
 // Response
-server.use(async (ctx) => {
-  ctx.body = 'Hello World';
-});
+// server.use(async (ctx) => {
+//   console.log('what is context', ctx);
+//   var context = JSON.stringify(ctx);
+//   ctx.body = `${context}`;
+// });
+server.use(serve('client/'))
 
 server.listen(port, () => console.log('Server started on port ', port));
 
