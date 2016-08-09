ad1e7a7af879ceabee666eb00d571175d05adcef
Derrick <djchie.dev@gmail.com>
2016-08-08 23:28:03 -0700

    Finshes the hacker news parser on the build endpoint

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 import Koa from 'koa';
 import serve from 'koa-static';
 
+import parseHackerNews from './parser/hacker-news-parser';
+
 const server = new Koa();
 const port = process.env.PORT || 3000;
 
 // Response
-// server.use(async (ctx) => {
-//   console.log('what is context', ctx);
-//   var context = JSON.stringify(ctx);
-//   ctx.body = `${context}`;
-// });
+server.use(async (ctx, next) => {
+  let items = await parseHackerNews();
+  ctx.body = {
+    items: items,
+  }
+});
+
 server.use(serve('client/'))
 
 server.listen(port, () => console.log('Server started on port ', port));
